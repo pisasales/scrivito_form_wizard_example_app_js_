4afc8f0183c4f46809d09d1439953e85e78fe82f
Amier El Ahmad <amier.elahmad@justrelate.com>
2023-09-20 09:41:16 +0200

    Remove unused

--- src/Widgets/FormContainerWidget/utils/validations/insideFormOrPageContainerValidation.js
+++
@@ -1,8 +0,0 @@
-import { getFormContainer } from "../getFormContainer";
-import { getPageContainer } from "../getPageContainer";
-
-export function insideFormContainerValidation(widget) {
-  if (!getFormContainer(widget) || !getPageContainer(widget)) {
-    return "Needs to be inside a form or a page.";
-  }
-}
